4de3464f5e1094061a63da54eb3a5e4f6071d11e
alaa <alaa.mady@cofomatique.com>
2023-04-05 23:06:11 +0200

    feat: register polls route

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+# polls/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PollViewSet
+
+router = DefaultRouter()
+router.register(r'polls', PollViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
