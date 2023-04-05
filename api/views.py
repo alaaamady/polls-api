57851ee0448f109832e0af35fc1f8c727741cce9
alaa <alaa.mady@cofomatique.com>
2023-04-05 23:00:50 +0200

    feat: create polls viewset

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Poll
+from .serializers import PollSerializer
 
-# Create your views here.
+
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description', 'choices__text']
+    ordering = ['-expiry_date']
+    paginate_by = 10
+    paginate_by_param = 'page_size'
+    max_paginate_by = 100
