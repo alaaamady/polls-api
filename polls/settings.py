5c3f017970fc91b4547bf3bb45f22fddb7d1201e
alaa <alaa.mady@cofomatique.com>
2023-04-06 10:03:49 +0200

    feat: deployment setup

--- polls/settings.py
+++ polls/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 
 env = environ.Env()
@@ -30,7 +31,7 @@ SECRET_KEY = 'django-insecure-&iv3zb&4g2syjrfwmx39(9%f6hy&d&ly^%*dv6($-vo-b!3g^3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
