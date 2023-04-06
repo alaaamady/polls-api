fb5b76798dce44d39b844e0b7665ae43a18a50c4
alaa <alaa.mady@cofomatique.com>
2023-04-06 18:15:17 +0200

    feat: return total vote count and choice vote count percentage

--- api/serializers.py
+++ api/serializers.py
@@ -12,14 +12,33 @@ class ChoiceSerializer(serializers.ModelSerializer):
 class PollSerializer(serializers.ModelSerializer):
     choices = ChoiceSerializer(many=True, read_only=True)
     expired = serializers.SerializerMethodField()
+    total_vote_count = serializers.SerializerMethodField()
+    choices_with_vote_percentage = serializers.SerializerMethodField()
 
     def get_expired(self, obj):
         return obj.expiry_date <= timezone.now()
 
+    def get_total_vote_count(self, obj):
+        return Vote.objects.filter(choice__poll=obj).count()
+
+    def get_choices_with_vote_percentage(self, obj):
+        choices = obj.choices.all()
+        vote_count = self.get_total_vote_count(obj)
+        data = []
+        for choice in choices:
+            choice_vote_count = choice.votes.count()
+            percentage = choice_vote_count / vote_count * 100 if vote_count > 0 else 0
+            data.append({
+                'choice_text': choice.text,
+                'vote_count': choice_vote_count,
+                'vote_percentage': percentage
+            })
+        return data
+
     class Meta:
         model = Poll
         fields = ['id', 'title', 'description',
-                  'choices', 'expired', 'expiry_date']
+                  'choices', 'expired', 'expiry_date', 'total_vote_count', 'choices_with_vote_percentage']
 
 
 class VoteSerializer(serializers.Serializer):
