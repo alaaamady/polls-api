c29175c98244f82e857edc6bec937701efb58355
alaa <alaa.mady@cofomatique.com>
2023-04-07 20:20:54 +0200

    fix: static files settings

--- polls/settings.py
+++ polls/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'rest_framework',
     'corsheaders',
-
+    'rest_framework_swagger',
 ]
 
 MIDDLEWARE = [
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'staticfiles/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
